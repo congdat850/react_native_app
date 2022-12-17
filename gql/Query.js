12f86f743ed3de3aaeeaaf4a0dec05d9bba2c7f1
Nguyen Cong Dat <nguyencongdat@Nguyens-MacBook-Air.local>
2022-12-17 09:59:36 +0700

    query findOne in grapql

--- gql/Query.js
+++ gql/Query.js
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+import { findNodeHandle } from "react-native";
 
 // export const CONTINENT_QUERY = gql`
 //   query ContinentQuery {
@@ -9,6 +10,7 @@ import { gql } from "@apollo/client";
 //   }
 // `;
 
+// 2. query find 
 export const CONTINENT_QUERY = gql`
 query {
     stores {
@@ -24,3 +26,47 @@ query {
     }
   }
 `;
+
+// 1. query findOne 
+
+// query {
+//   stores(filters:{name: {eq: "iphone 11"}}) {
+//     data{
+//       id,
+//       attributes {
+//         name,
+//         describe,
+//         price,
+//         image
+//       }
+//     }
+//   }
+// }
+
+// 2. query find 
+
+// query {
+//   stores {
+//     data{
+//       id,
+//       attributes {
+//         name,
+//         describe,
+//         price,
+//         image
+//       }
+//     }
+//   }
+// }
+
+// mutation updateArticle {
+//   updateArticle(id: "1", data: { title: "Hello" }) {
+//     data {
+//       id
+//       attributes {
+//         title
+//       }
+//     }
+//   }
+// }
+
